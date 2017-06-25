0798d6d5c22a13bc733e691999c6941cd62e4512
Bobby <bobbychuong@gmail.com>
2017-06-24 22:23:12 -0700

    odd numbers

--- algo.js
+++ algo.js
@@ -1,3 +1,11 @@
+// Print from 1-255
 for(var x=0;x<=255;x++){
   console.log(x);
+}
+// Assignment: Odd Numbers
+// Write a program that would print all the odd numbers from 1 to 1000
+for(var x=0;x<=1000;x++){
+  if(x%2 ==1){
+    console.log(x);
+  }
 }
