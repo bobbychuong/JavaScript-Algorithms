9fa1c98c564b2c5d77e9c9c6c3b994ccabe40e25
Bobby <bobbychuong@gmail.com>
2017-06-27 00:23:31 -0700

    Greater than Y

--- algo.js
+++ algo.js
@@ -17,13 +17,50 @@
 //   }
 // }
 // console.log(sum);
+// Iterating through the array
+// var x = [1,3,5,7,9,13];
+// for (var i=0;i<x.length;i++){
+//   console.log(x[i]);
+// }
+
+// Find Max
+// var arr=[-3, 3, 5, 7,10,1000000,-10000];
+// var max= -3;
+// for (var x=0;x<arr.length;x++){
+//     if(arr[x] > max){                    //if(3 > -3)    => if(arr[0] > max)
+//         max = arr[x];                    // max = arr[1] => max = 3
+//     }
+// }
+// console.log (max)
+
+// // Find average
+// var arr = [1,3,5,7,20];
+// var sum = 0;
+// for (var x=0;x<arr.length;x++){
+//     sum = sum + arr[x];
+// }
+// console.log(sum/arr.length);
+
 // Assignment: Array With Odd Numbers
 // Write a program that creates an array 'Y' that contains all the odd numbers between 1 to 255. When the program is done, 'y'
 // should have the value of [1, 3, 5, 7, ... 255]. You are allowed to use .push method)
-var y = [];
-for (var x=1;x<=256;x++){
-  if(x%2==1){
-    y.push(x);
-    console.log(x);
-  }
+// var y = [];
+// for (var x=0;x<=256;x++){
+//   if(x%2==1){
+//     y.push(x);
+//   }
+//   console.log(y);
+// }
+
+function greaterThanY(y){
+    var arr = [1,3,5,7];
+    var newarr =[]
+    var count = 0;
+    for(var x=0;x<arr.length;x++){
+        if(arr[x] > y){
+          count++;
+        }
+    }
+    console.log(count);
 }
+greaterThanY(6);
